b5b88d7da79fcb05c714e53587d1a39a1919d432
ymbelyakova <ymbelyakova@gmail.com>
2026-01-11 20:05:58 +0300

    Ex18: Тесты на DELETE

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -4,8 +4,7 @@ from lib.my_requests import MyRequests
 
 class TestUserDelete(BaseCase):
 
-    # Успешное удаление нового созданного пользователя
-    def test_user_delete_successfully(self):
+    def setup_method(self):
         # REGISTER
         register_data = self.prepare_registration_data()
         response1 = MyRequests.post("/user/", data=register_data)
@@ -13,26 +12,33 @@ class TestUserDelete(BaseCase):
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
 
-        email = register_data["email"]
-        password = register_data["password"]
-        user_id = self.get_json_value(response1, "id")
+        self.email = register_data["email"]
+        self.password = register_data["password"]
+        # self.username = register_data["username"]
+        # self.lastName = register_data["lastName"]
+        self.firstName = register_data["firstName"]
+        self.user_id = self.get_json_value(response1, "id")
 
         # LOGIN
         login_data = {
-            "email": email,
-            "password": password
+            "email": self.email,
+            "password": self.password
         }
 
         response2 = MyRequests.post("/user/login", data=login_data)
 
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
+        self.auth_sid = self.get_cookie(response2, "auth_sid")
+        self.token = self.get_header(response2, "x-csrf-token")
+
+
+    # Успешное удаление нового созданного пользователя
+    def test_user_delete_successfully(self):
 
         # DELETE
         response3 = MyRequests.delete(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
+            f"/user/{self.user_id}",
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
         )
 
         Assertions.assert_code_status(response3, 200)
@@ -45,10 +51,11 @@ class TestUserDelete(BaseCase):
 
         # GET
         response4 = MyRequests.get(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
+            f"/user/{self.user_id}",
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
         )
+
 
         Assertions.assert_code_status(response4, 404)
         assert response4.content.decode("utf-8") == "User not found", f"Unexpected error message {response4.content}"
@@ -85,6 +92,7 @@ class TestUserDelete(BaseCase):
         )
 
         # GET
+        expected_fields = ["id", "username", "email", "firstName", "lastName"]
         response3 = MyRequests.get(
             "/user/2",
             headers={"x-csrf-token": token},
@@ -92,52 +100,23 @@ class TestUserDelete(BaseCase):
         )
 
         Assertions.assert_code_status(response3, 200)
-        Assertions.assert_json_has_keys(response3, ["id", "username", "email", "firstName", "lastName"])
+        Assertions.assert_json_has_keys(response3, expected_fields)
 
-        # Невозможно удалить пользователя будучи авторизованным другим пользователем
-
+    # Невозможно удалить пользователя будучи авторизованным другим пользователем
     def test_user_delete_not_own_user_negative(self):
-        # REGISTER
-        register_data = self.prepare_registration_data()
-        response1 = MyRequests.post("/user/", data=register_data)
-
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
-
-        user_id = self.get_json_value(response1, "id")
-
-        # LOGIN
-        login_data = {
-            "email": 'vinkotov@example.com',
-            "password": '1234'
-        }
-
-        response2 = MyRequests.post("/user/login", data=login_data)
-        auth_sid = self.get_cookie(response2, "auth_sid")
-        token = self.get_header(response2, "x-csrf-token")
 
         # DELETE
         response3 = MyRequests.delete(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
+            f"/user/10",
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
         )
 
         Assertions.assert_code_status(response3, 400)
         Assertions.assert_json_value_by_name(
             response3,
             "error",
-            "Please, do not delete test users with ID 1, 2, 3, 4 or 5.",
+            "This user can only delete their own account.",
             f"Unexpected error message {response3.content}"
         )
-
-        # GET
-        response4 = MyRequests.get(
-            f"/user/{user_id}",
-            headers={"x-csrf-token": token},
-            cookies={"auth_sid": auth_sid}
-        )
-
-        Assertions.assert_code_status(response4, 200)
-        Assertions.assert_json_has_key(response4, "username")
 
